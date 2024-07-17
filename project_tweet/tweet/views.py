e033a62ac936c564fc72e846cdff3fce82454d47
m-kazi <kazzcode@gmail.com>
2024-07-17 16:16:16 +0600

    views - CRUD operations

--- project_tweet/tweet/views.py
+++ project_tweet/tweet/views.py
@@ -1,6 +1,66 @@
 from django.shortcuts import render
+from .models import Tweet
+from .forms import TweetForm
+from django.shortcuts import get_object_or_404, redirect
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
+
+# to get all the tweets
+
+
+def tweet_list(request):
+    tweet = Tweet.objects.all().order_by("-created_at")
+    return render(request, "tweet_list.html", {"tweet": tweet})
+
+
+# to create tweets
+
+
+def tweet_create(request):
+    if request.method == "POST":
+        form = TweetForm(request.POST, request.FILES)
+        if form.is_valid():
+            # save the tweet if it's valid but not on the DB
+            tweet = form.save(commit=False)
+            # get the user who tweeted
+            tweet.user = request.user
+            # save in the DB
+            tweet.save()
+            return redirect("tweet_list")
+    else:
+        form = TweetForm()
+
+    return render(request, "tweet_form.html", {"form": form})
+
+
+# to edit the tweet, always 'instance'
+
+
+def tweet_edit(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+    if request.method == "POST":
+        form = TweetForm(request.POST, request.FILES, instance=tweet)
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect("tweet_list")
+    else:
+        form = TweetForm(instance=tweet)
+    return render(request, "tweet_form.html", {"form": form})
+
+
+# to delete the tweet
+
+
+def tweet_delete(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+    if request.method == "POST":
+        tweet.delete()
+        return redirect("tweet_list")
+    return render(request, "tweet_confirm_delete.html", {"tweet": tweet})
